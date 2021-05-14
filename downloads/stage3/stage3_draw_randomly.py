d4fb40c69b1bd9af0a60ffa670035c0c0e25e27e
mdecourse <mdecourse@gmail.com>
2021-05-14 16:18:57 +0800

    add stage3 links

---
+++ downloads/stage3/stage3_draw_randomly.py
@@ -0,0 +1,53 @@
+# open file, default is read mode, since txt content no chinese char
+# no encoding = "UTF-8" is needed
+import random
+
+# number of group menber to draw
+num = 4
+
+# check if data is "" or not
+def notVacant(data):
+    if data == "":
+        return False
+    else:
+        return True
+        
+with open("stage3_2b.txt") as fh:
+    # readlines will read into the whole line and put into list format
+    # has \n at the end of each line
+    data = fh.readlines()
+#print(len(data))
+# big group list
+bgroup = []
+# count from the second group member
+sgroup = []
+for i in range(len(data)):
+    group = data[i].rstrip("\n").split("\t")
+    #print(group)
+    # use mem to count the total number of each group
+    mem = 0
+    # final group data
+    fgroup = []
+    # count from the second group member, eliminate the first element
+    sgroup = group[1:]
+    # get only the odd index number
+    igroup = [i for i in range(len(sgroup)) if i % 2 == 1]
+    for j in igroup:
+        # index starts from 0 which is j-1 when j=1
+        if notVacant(sgroup[j-1]) == True:
+            mem += 1
+            fgroup.append(sgroup[j-1])
+    print("group " + str(i+1) + ":" + str(mem))
+    # shuffle the fgroup list
+    random.shuffle(fgroup)
+    drawEd = []
+    # draw num of member from final group list: fgroup
+    for k in range(num):
+        try:
+            print(fgroup[k])
+        except:
+            # num is greater than total number of this group
+            print("no such member")
+    # seperator
+    print("-"*20)
+# the following will use group data to generate needed html
--- downloads/stage3/stage3_gen_html.py
+++ downloads/stage3/stage3_gen_html.py
@@ -1,9 +1,6 @@
-# open file, default is read mode, since txt content no chinese char
-# no encoding = "UTF-8" is needed
-import random
-
-# number of group menber to draw
-num = 2
+# stage3_gen_html.py
+studList = "stage3_2a.txt"
+courTitle = "cd2021"
 
 # check if data is "" or not
 def notVacant(data):
@@ -11,42 +8,60 @@ def notVacant(data):
         return False
     else:
         return True
-        
-with open("stage3_2a.txt") as fh:
+
+# given group and generate needed html
+# group title initial
+grpTitle = studList[:6] + "-" + studList[8] + "g"
+
+# student Account and github Account as inputs
+def perWeb(s, g):
+    pSite = "<a href=\"https://" + g + ".github.io/" + courTitle + "\">" + s + " web</a>"
+    print(pSite)
+
+def perRepo(s, g):
+    pSite = "<a href=\"https://github.com/" + g + "/" + courTitle + "\">" + s + " repo</a>"
+    print(pSite)
+    
+def grpWeb(s, g, ord):
+    pSite = "<a href=\"https://" + g + ".github.io/" + grpTitle + str(ord) + "\">grp web</a>"
+    print(pSite)
+    
+def grpRepo(s, g, ord):
+    pSite = "<a href=\"https://github.com/" + g + "/" + grpTitle + str(ord) + "\">grp repo</a>"
+    print(pSite)
+    
+def genHtml(group, grpTitle):
+    # total number of members in each group
+    total = int(len(group[1:])/2)
+    for i in range(total):
+        print(grpTitle, group[i])
+
+# open file, default is read mode, since txt content no chinese char
+# no encoding = "UTF-8" is needed
+with open(studList) as fh:
     # readlines will read into the whole line and put into list format
     # has \n at the end of each line
     data = fh.readlines()
 #print(len(data))
-# big group list
-bgroup = []
-# count from the second group member
-sgroup = []
+grpCount = 0
 for i in range(len(data)):
+    grpCount = grpCount + 1
     group = data[i].rstrip("\n").split("\t")
-    #print(group)
-    # use mem to count the total number of each group
-    mem = 0
-    # final group data
-    fgroup = []
-    # count from the second group member, eliminate the first element
-    sgroup = group[1:]
-    # get only the odd index number
-    igroup = [i for i in range(len(sgroup)) if i % 2 == 1]
-    for j in igroup:
-        # index starts from 0 which is j-1 when j=1
-        if notVacant(sgroup[j-1]) == True:
-            mem += 1
-            fgroup.append(sgroup[j-1])
-    print("group " + str(i+1) + ":" + str(mem))
-    # shuffle the fgroup list
-    random.shuffle(fgroup)
-    # draw num of member from final group list: fgroup
-    for k in range(num):
-        try:
-            print(fgroup[k])
-        except:
-            # num is greater than total number of this group
-            print("no such member")
-    # seperator
-    print("-"*20)
-# the following will use group data to generate needed html
+    # drop the first element and vacant element
+    # use filter to remove vacant string element
+    # use [1:] to remove first element
+    group = list(filter(None, group))[1:]
+    # 01, 23, 45, 67
+    print(grpTitle + str(grpCount) + "<br />")
+    for j in range(0, len(group), 2):
+        #print(j)
+        s = group[j]
+        g = group[j+1]
+        perWeb(s, g)
+        print("|")
+        perRepo(s, g)
+        print("|")
+        grpWeb(s, g, grpCount)
+        print("|")
+        grpRepo(s, g, grpCount)
+        print("<br />")
